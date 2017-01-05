4f0626c6ee781e913f8c4f4e0f0350e654223513
ddionisio <garapata@gmail.com>
2017-01-04 19:10:59 -0800

    setting up UI infrastructure

---
+++ Assets/Scripts/UI/ModalOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using M8.UIModal.Interface;
+using UnityEngine;
+
+public class ModalOptions : M8.UIModal.Controller, IPush, IPop {
+    void IPush.Push(M8.GenericParams parms) {
+        M8.SceneManager.instance.Pause();
+    }
+
+    void IPop.Pop() {
+        M8.SceneManager.instance.Resume();
+    }
+}
